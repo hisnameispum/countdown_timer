e28f6fbb916e9eafdedeb3ad801ff684e34e6b59
hisnameispum <sirasitxp@gmail.com>
2020-12-09 02:46:42 -0800

    Added timer functionality

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,12 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:percent_indicator/circular_percent_indicator.dart';
 
 void main() => runApp(MaterialApp(
-  debugShowCheckedModeBanner: false,
-  home: Pomodoro(),
-));
+      debugShowCheckedModeBanner: false,
+      home: Pomodoro(),
+    ));
 
 class Pomodoro extends StatefulWidget {
   @override
@@ -15,17 +17,46 @@ class _PomodoroState extends State<Pomodoro> {
   double percent = 0;
   static int timeInMinute = 25;
   int timeInSeconds = timeInMinute * 60;
+  Timer timer;
+  _startTimer(){
+    timeInMinute = 25;
+    int time = timeInMinute*60;
+    double secPercent = (time/100);
+    timer = Timer.periodic(Duration(seconds: 1), (timer) {
+      setState(() {
+        if (time > 0 ){
+          time--;
+          if (time % 60 == 0){
+            timeInMinute--;
+          }
+          if (time % secPercent == 0){
+            if (percent < 1){
+              percent += 0.01;
+            }
+            else{
+              percent = 1;
+            }
+          }
+        }
+        else{
+          percent = 0;
+          timeInMinute = 25;
+          timer.cancel();
+        }
+      });
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
         body: Container(
           decoration: BoxDecoration(
-            gradient: LinearGradient(
-              colors: [Color(0xff1542bf), Color(0xff51abff)],
-              begin: FractionalOffset(0.5,1),
-            )
-          ),
+              gradient: LinearGradient(
+            colors: [Color(0xff1542bf), Color(0xff51abff)],
+            begin: FractionalOffset(0.5, 1),
+          )),
           width: double.infinity,
           child: Column(
             mainAxisAlignment: MainAxisAlignment.start,
@@ -57,7 +88,100 @@ class _PomodoroState extends State<Pomodoro> {
                       fontSize: 60,
                     ),
                   ),
-
+                ),
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              Expanded(
+                child: Container(
+                  width: double.infinity,
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.only(
+                        topRight: Radius.circular(30),
+                        topLeft: Radius.circular(30),
+                    ),
+                  ),
+                  child: Padding(
+                    padding: EdgeInsets.only(
+                      top: 30,
+                      left: 20,
+                      right: 20,
+                    ),
+                    child: Column(
+                      children: [
+                        Expanded(
+                          child: Row(
+                            children: [
+                              Expanded(
+                                child: Column(
+                                  children: [
+                                    Text(
+                                      "Study Time",
+                                      style: TextStyle(
+                                        fontSize: 30,
+                                      ),
+                                    ),
+                                    SizedBox(
+                                      height: 10,
+                                    ),
+                                    Text(
+                                      "25",
+                                      style: TextStyle(
+                                        fontSize: 60,
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                              ),
+                              Expanded(
+                                child: Column(
+                                  children: [
+                                    Text(
+                                      "Break Time",
+                                      style: TextStyle(
+                                        fontSize: 30,
+                                      ),
+                                    ),
+                                    SizedBox(
+                                      height: 10,
+                                    ),
+                                    Text(
+                                      "5",
+                                      style: TextStyle(
+                                        fontSize: 60,
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.symmetric(vertical: 28),
+                          child: RaisedButton(
+                            onPressed: _startTimer,
+                            color: Colors.blue,
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(100),
+                            ),
+                            child: Padding(
+                              padding: EdgeInsets.all(20.0),
+                              child: Text(
+                                "Start Studying",
+                                style: TextStyle(
+                                  color: Colors.white,
+                                  fontSize: 22,
+                                ),
+                              ),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
                 ),
               ),
             ],
